3c4b1316ff8bc50583651e6f0518b55ffd139425
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-28 22:54:30 +0530

    Adding an initial fish config.

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/state"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
