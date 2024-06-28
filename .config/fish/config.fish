1f847faf1454f095e02491a987a62bbc21614715
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-28 23:11:19 +0530

    Minor spelling mistake.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,4 @@
 # set -gx XDG_DATA_HOME "$HOME/.local/state"
 # set -gx XDG_CACHE_HOME "$HOME/.cache"
 
-set -g fish_key_bindings fish_vi_key_bindingst 
+set -g fish_key_bindings fish_vi_key_bindings 
