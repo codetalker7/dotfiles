2ce625b544119735e1a51be9de305ed3ef60673c
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-29 22:06:07 +0530

    Adding lilypond suite to nvim.

---
+++ .config/nvim/lua/plugins/nvim_lilypond_suite.lua
@@ -0,0 +1,13 @@
+return { 
+    'martineausimon/nvim-lilypond-suite',
+    config = function()
+        require('nvls').setup({
+            -- edit config here (see "Customize default settings" in wiki)
+            lilypond = {
+                options = {
+                    pdf_viewer = 'zathura',
+                }
+            }
+        })
+  end
+}
