d92889ff176a69849c21eb54e104e78fcad70742
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-29 18:42:46 +0530

    Defining XDG variables.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,9 @@
 #!/usr/bin/env fish
 
-# set -gx XDG_DATA_HOME "$HOME/.local/share"
-# set -gx XDG_CONFIG_HOME "$HOME/.config"
-# set -gx XDG_DATA_HOME "$HOME/.local/state"
-# set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/state"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
 
 set -g fish_key_bindings fish_vi_key_bindings 
 
