889348c8ef7158338d1b587f8a61aea49e7ab45b
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-29 21:26:36 +0530

    Some config for vim_markdown.

--- .config/nvim/lua/plugins/vim_markdown.lua
+++ .config/nvim/lua/plugins/vim_markdown.lua
@@ -2,4 +2,10 @@ return {
     "preservim/vim-markdown",
     version = "*",
     lazy = false,
+    config = function ()
+        vim.cmd [[
+            let g:vim_markdown_math = 1
+            let g:vim_markdown_folding_disabled = 1
+        ]]
+    end,
 }
