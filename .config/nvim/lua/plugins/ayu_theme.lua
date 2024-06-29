ca10910cf5fd2435e07dbfc22d0c69d96e7c414c
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-29 22:24:02 +0530

    Minor change.

--- .config/nvim/lua/plugins/ayu_theme.lua
+++ .config/nvim/lua/plugins/ayu_theme.lua
@@ -3,8 +3,6 @@ return {
     config = function()
         vim.cmd [[
             set termguicolors     " enable true colors support
-            let ayucolor="light"  " for light version of theme
-            let ayucolor="mirage" " for mirage version of theme
             let ayucolor="dark"   " for dark version of theme
             colorscheme ayu
         ]]
