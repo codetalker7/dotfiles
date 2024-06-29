11a9381179443832736fffb326503df886943600
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-29 22:23:39 +0530

    Adding the ayu theme.

---
+++ .config/nvim/lua/plugins/ayu_theme.lua
@@ -0,0 +1,12 @@
+return {
+    "ayu-theme/ayu-vim",
+    config = function()
+        vim.cmd [[
+            set termguicolors     " enable true colors support
+            let ayucolor="light"  " for light version of theme
+            let ayucolor="mirage" " for mirage version of theme
+            let ayucolor="dark"   " for dark version of theme
+            colorscheme ayu
+        ]]
+    end,
+}
