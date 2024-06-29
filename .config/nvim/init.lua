5e8e8eeb3af91a050f610bf582fef29e7774333b
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-29 18:23:53 +0530

    Adding an init.lua.

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+--some indentation settings
+vim.o.expandtab = true                          -- on pressing tab, insert spaces
+vim.o.smartindent = true                        
+vim.o.tabstop = 4                               -- number of spaces to insert on tab
+vim.o.shiftwidth = 4                            -- number of spaces per indent level
+
+--some search settings
+vim.o.hlsearch = true                           -- highlight text during search
