fd816cb69bded79a15d4d5eeef01fd4a3629d541
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-29 19:00:06 +0530

    Installing packer, and adding a plugins.lua.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,22 @@
---some indentation settings
+-- disable netrw at the very start of your init.lua; for nvim-tree.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- some indentation settings
 vim.o.expandtab = true                          -- on pressing tab, insert spaces
 vim.o.smartindent = true                        
 vim.o.tabstop = 4                               -- number of spaces to insert on tab
 vim.o.shiftwidth = 4                            -- number of spaces per indent level
 
---some search settings
+-- some search settings
 vim.o.hlsearch = true                           -- highlight text during search
+vim.keymap.set('n', 'CR', ':noh<CR><CR>')       -- remove the search highlight; see :help noh
+
+-- line numbers
+vim.o.nu = true
+
+-- enable syntax, if it isn't
+vim.cmd('syntax enable')
+
+-- load plugins
+require("plugins")
