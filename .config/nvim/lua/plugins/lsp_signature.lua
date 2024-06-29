3cc33dde1df863e7e9717169cb4549c7386a817d
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-29 22:55:30 +0530

    Adding lspsaga.

--- .config/nvim/lua/plugins/lsp_signature.lua
+++ .config/nvim/lua/plugins/lsp_signature.lua
@@ -1,5 +1,7 @@
 return {
     "ray-x/lsp_signature.nvim",
+    version = "*",
+    lazy = false,
     event = "VeryLazy",
     opts = {},
     config = function(_, opts) require("lsp_signature").setup(opts) end
---
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,14 @@
+return {
+    "nvimdev/lspsaga.nvim",
+    version = "*",
+    lazy = false,
+    config = function()
+        require('lspsaga').setup({})
+    end,
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter", -- optional
+        "nvim-tree/nvim-web-devicons",     -- optional
+        "neovim/nvim-lspconfig",
+    }
+
+}
