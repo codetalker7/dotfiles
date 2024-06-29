8010ca9ebc84764b451c80fd2ae46a9e8867cbaa
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-29 20:07:12 +0530

    Adding nvim-treesitter config.

---
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,33 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    version = "*",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function () 
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = {
+                "bibtex",
+                "c",
+                "cuda",
+                "go",
+                "html",
+                "java",
+                "julia",
+                "latex",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "rust",
+                "vim",
+                "vimdoc",
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },  
+        })
+    end,
+}
