3ff47c4ba6064bb043222ef549604df8e6e40aa9
Siddhant Chaudhary <urssidd@gmail.com>
2024-06-29 20:35:31 +0530

    Adding lualine.

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,15 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("lualine").setup {
+            options = {
+                theme = "dracula",
+            }
+        }
+    end,
+}
