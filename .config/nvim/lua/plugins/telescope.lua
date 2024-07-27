34a105be382546b7e5447caf665beb54ca805653
Siddhant Chaudhary <urssidd@gmail.com>
2024-07-27 13:02:29 +0530

    Adding a mapping for telescope.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,5 +3,9 @@ return {
 -- or                              , branch = '0.1.x',
     version = "*",
     lazy = false,
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        -- vim.api.nvim_set_keymap("", "T", ":Telescope")
+        vim.api.nvim_set_keymap("", "T", ":Telescope<CR>", { noremap = true })
+    end,
 }
