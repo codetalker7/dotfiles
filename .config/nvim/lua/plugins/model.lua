7ff807cc1527123f944ca075828bafab5a4c1f44
Siddhant Chaudhary <urssidd@gmail.com>
2024-09-01 19:51:16 +0530

    Adding llama3.1 template format to model.nvim.

--- .config/nvim/lua/plugins/model.lua
+++ .config/nvim/lua/plugins/model.lua
@@ -9,6 +9,40 @@ return {
 			return context.selection and input or ""
 		end
 
+		llama_fmt = {
+            -- taken from https://github.com/meta-llama/llama-recipes
+			---@param messages ChatMessage[]
+			---@param config ChatConfig
+			chat = function(messages, config)
+				if #messages < 1 then
+					error("Need at least one message")
+				end
+
+				local first_msg = messages[1]
+				local prompt = ""
+				if config.system then
+                    prompt = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n"
+                        .. config.system .. "<eot_id>"
+				end
+
+				for _, msg in ipairs(messages) do
+					prompt = prompt
+						.. (msg.role == "user" and "<|start_header_id|>user<|end_header_id|>\n" or "<|start_header_id|>assistant<|end_header_id|>\n")
+						.. msg.content .. "<eot_id>"
+				end
+
+				prompt = prompt .. "<|start_header_id|>assistant<|end_header_id|>\n"
+
+                print(prompt)
+
+				return {
+					prompt = prompt,
+					raw = true,
+				}
+			end,
+		}
+
+
 		codegemma_fmt = {
 			---@param messages ChatMessage[]
 			---@param config ChatConfig
@@ -41,6 +75,14 @@ return {
 
 		require("model").setup({
 			chats = {
+				general = {
+					provider = ollama,
+					params = {
+						model = "llama3.1:8b",
+					},
+					create = input_if_selection,
+					run = llama_fmt.chat,
+				},
 				code = {
 					provider = ollama,
 					params = {
