38b278c757afe9b475d1bb9e34c5c7c964c91014
Siddhant Chaudhary <urssidd@gmail.com>
2024-09-19 02:49:12 +0530

    Adding my own groq provider for model.nvim (I'm using my own fork of the plugin).

--- .config/nvim/lua/plugins/model.lua
+++ .config/nvim/lua/plugins/model.lua
@@ -4,13 +4,20 @@ return {
 	lazy = false,
 	ft = "mchat",
 	config = function()
+		-- ollama
 		local ollama = require("model.providers.ollama")
+
+		-- groq
+		local groq = require("model.providers.groq_cloud")
+		local groq_fmt = require("model.format.groq_cloud")
+
+		-- default builder for chats
 		local function input_if_selection(input, context)
 			return context.selection and input or ""
 		end
 
 		llama_fmt = {
-            -- taken from https://github.com/meta-llama/llama-recipes
+			-- taken from https://github.com/meta-llama/llama-recipes
 			---@param messages ChatMessage[]
 			---@param config ChatConfig
 			chat = function(messages, config)
@@ -21,19 +28,22 @@ return {
 				local first_msg = messages[1]
 				local prompt = ""
 				if config.system then
-                    prompt = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n"
-                        .. config.system .. "<eot_id>"
+					prompt = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n"
+						.. config.system
+						.. "<eot_id>"
 				end
 
 				for _, msg in ipairs(messages) do
 					prompt = prompt
 						.. (msg.role == "user" and "<|start_header_id|>user<|end_header_id|>\n" or "<|start_header_id|>assistant<|end_header_id|>\n")
-						.. msg.content .. "<eot_id>"
+						.. msg.content
+						.. "<eot_id>"
 				end
 
-				prompt = prompt .. "<|start_header_id|>assistant<|end_header_id|>\n"
+				prompt = prompt
+					.. "<|start_header_id|>assistant<|end_header_id|>\n"
 
-                print(prompt)
+				print(prompt)
 
 				return {
 					prompt = prompt,
@@ -41,7 +51,6 @@ return {
 				}
 			end,
 		}
-
 
 		codegemma_fmt = {
 			---@param messages ChatMessage[]
@@ -54,7 +63,9 @@ return {
 				local first_msg = messages[1]
 				local prompt = ""
 				if config.system then
-					prompt = "<start_of_turn>user\n" .. config.system .. "<end_of_turn>\n"
+					prompt = "<start_of_turn>user\n"
+						.. config.system
+						.. "<end_of_turn>\n"
 				end
 
 				for _, msg in ipairs(messages) do
@@ -75,7 +86,7 @@ return {
 
 		require("model").setup({
 			chats = {
-				general = {
+				["ollama:llama3.1:8b"] = {
 					provider = ollama,
 					params = {
 						model = "llama3.1:8b",
@@ -83,13 +94,23 @@ return {
 					create = input_if_selection,
 					run = llama_fmt.chat,
 				},
-				code = {
+				["ollama:codegemma:7b"] = {
 					provider = ollama,
 					params = {
 						model = "codegemma:7b",
 					},
 					create = input_if_selection,
 					run = codegemma_fmt.chat,
+				},
+				["groq:llama3-8b-8192"] = {
+					provider = groq,
+					options = {
+						model = "llama3-8b-8192",
+					},
+					create = function(input, context)
+						return context.selection and input or ""
+					end,
+					run = groq_fmt.chat,
 				},
 			},
 		})
