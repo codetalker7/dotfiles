3a151984d53afd12dbb17649e1a2959b5770cc1b
Siddhant Chaudhary <urssidd@gmail.com>
2025-06-05 22:30:03 +0530

    Adding clang-format to nvim's formatter.nvim.

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -10,12 +10,22 @@ return {
 			local s_start = vim.fn.getpos("'<")
 			local s_end = vim.fn.getpos("'>")
 			local n_lines = math.abs(s_end[2] - s_start[2]) + 1
-			local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+			local lines = vim.api.nvim_buf_get_lines(
+				0,
+				s_start[2] - 1,
+				s_end[2],
+				false
+			)
 			lines[1] = string.sub(lines[1], s_start[3], -1)
 			if n_lines == 1 then
-				lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+				lines[n_lines] = string.sub(
+					lines[n_lines],
+					1,
+					s_end[3] - s_start[3] + 1
+				)
 			else
-				lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+				lines[n_lines] =
+					string.sub(lines[n_lines], 1, s_end[3])
 			end
 			return table.concat(lines, "\n")
 		end -- get the selected lines
@@ -34,7 +44,9 @@ return {
 							exe = "stylua",
 							cwd = pwd,
 							args = {
-								util.escape_path(util.get_current_buffer_file_path()),
+								util.escape_path(
+									util.get_current_buffer_file_path()
+								),
 							},
 							stdin = false,
 						}
@@ -50,7 +62,9 @@ return {
 							args = {
 								"--project=dev/",
 								"dev/format.jl",
-								util.escape_path(util.get_current_buffer_file_path()),
+								util.escape_path(
+									util.get_current_buffer_file_path()
+								),
 							},
 							stdin = false,
 						}
@@ -65,7 +79,9 @@ return {
 							cwd = pwd,
 							args = {
 								"format",
-								util.escape_path(util.get_current_buffer_file_path()),
+								util.escape_path(
+									util.get_current_buffer_file_path()
+								),
 							},
 							stdin = false,
 						}
@@ -80,7 +96,90 @@ return {
 							cwd = pwd,
 							args = {
 								"format",
-								util.escape_path(util.get_current_buffer_file_path()),
+								util.escape_path(
+									util.get_current_buffer_file_path()
+								),
+							},
+							stdin = false,
+						}
+					end,
+				},
+				rmd = {
+					function()
+						-- Full specification of configurations is down below and in Vim help
+						-- files. or just read the docs on github
+						return {
+							exe = "deno fmt",
+							cwd = pwd,
+							args = {
+								"format",
+								util.escape_path(
+									util.get_current_buffer_file_path()
+								),
+							},
+							stdin = false,
+						}
+					end,
+				},
+				c = {
+					function()
+						-- Full specification of configurations is down below and in Vim help
+						-- files. or just read the docs on github
+						return {
+							exe = "clang-format -i -style webkit",
+							cwd = pwd,
+							args = {
+								util.escape_path(
+									util.get_current_buffer_file_path()
+								),
+							},
+							stdin = false,
+						}
+					end,
+				},
+				cpp = {
+					function()
+						-- Full specification of configurations is down below and in Vim help
+						-- files. or just read the docs on github
+						return {
+							exe = "clang-format -i -style webkit",
+							cwd = pwd,
+							args = {
+								util.escape_path(
+									util.get_current_buffer_file_path()
+								),
+							},
+							stdin = false,
+						}
+					end,
+				},
+				h = {
+					function()
+						-- Full specification of configurations is down below and in Vim help
+						-- files. or just read the docs on github
+						return {
+							exe = "clang-format -i -style webkit",
+							cwd = pwd,
+							args = {
+								util.escape_path(
+									util.get_current_buffer_file_path()
+								),
+							},
+							stdin = false,
+						}
+					end,
+				},
+				hpp = {
+					function()
+						-- Full specification of configurations is down below and in Vim help
+						-- files. or just read the docs on github
+						return {
+							exe = "clang-format -i -style webkit",
+							cwd = pwd,
+							args = {
+								util.escape_path(
+									util.get_current_buffer_file_path()
+								),
 							},
 							stdin = false,
 						}
