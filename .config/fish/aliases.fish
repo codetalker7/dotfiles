99aaac4249472164ef01f2aeec58a7af037d1a22
Siddhant Chaudhary <urssidd@gmail.com>
2025-06-08 00:38:51 +0530

    Adding more aliases.

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -8,4 +8,17 @@ function lsd -d 'List only directories (in the current dir)'
     ls -d */ | sed -Ee 's,/+$,,'
 end
 
+# git
 alias g git
+alias gst="git status"
+alias ga="git add"
+alias gc="git commit -v"
+alias gp="git push"
+alias gl="git pull"
+alias gd="git diff"
+
+# system and utils
+alias md="mkdir -p"        # Make directory (parent-safe)
+
+# fish shell
+alias config="nvim ~/.config/fish/config.fish"
