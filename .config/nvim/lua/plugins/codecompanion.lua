0cd315bbbb43b8e700f6cbd4310ffddcca850607
Siddhant Chaudhary <urssidd@gmail.com>
2025-06-07 20:47:59 +0530

    Removing unnecessary plugins.

---
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,9 @@
+return {
+	"olimorris/codecompanion.nvim",
+	lazy = false,
+	version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+}
