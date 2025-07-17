3c9728398ddf71281a6ee75334fa60da80900bd3
Siddhant Chaudhary <urssidd@gmail.com>
2025-07-17 20:19:53 +0530

    minor fix

--- .config/i3/spawn_coding_workspace.fish
+++ .config/i3/spawn_coding_workspace.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
 
-i3-msg "workspace $argv[1]; append_layout ~/.config/i3/coding_workspace.json" & alacritty --class CodingNvimTerm,codingnvimterm -e fish -c "cd /home/codetalker7/coding && tmux new-session -A -s coding 'nvim; fish'" & alacritty --class BookdownTerm,bookdownterm -e fish -c "cd /home/codetalker7/coding && R; exec fish" & zathura
+i3-msg "workspace $argv[1]; append_layout ~/.config/i3/coding_workspace.json" & alacritty --class CodingNvimTerm,codingnvimterm -e fish -c "cd /home/codetalker7/coding && tmux -u new-session -A -s coding 'nvim; fish'" & alacritty --class BookdownTerm,bookdownterm -e fish -c "cd /home/codetalker7/coding && R; exec fish" & zathura
