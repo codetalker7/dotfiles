ba7b913f670c88166df93605f13950779a6fa1f2
Siddhant Chaudhary <urssidd@gmail.com>
2026-05-09 11:42:34 +0000

    updating the notes

--- utahchpc/ubuntu2204.apptainer.sh
+++ utahchpc/ubuntu2204.apptainer.sh
@@ -2,24 +2,26 @@
 # do this in an interactive job with salloc
 cd /scratch/general/vast/u1591895
 module load apptainer
-apptainer build --sandbox ubuntu2204-sbx docker://ubuntu:latest
-mkdir ubuntu2204-sbx/uufs
-touch ubuntu2204-sbx/etc/localtime
-mkdir -p ubuntu2204-sbx/uufs/chpc.utah.edu
-mkdir ubuntu2204-sbx/scratch
+apptainer build --sandbox ubuntulatest-sbx docker://ubuntu:latest
+mkdir ubuntulatest-sbx/uufs
+touch ubuntulatest-sbx/etc/localtime
+mkdir -p ubuntulatest-sbx/uufs/chpc.utah.edu
+mkdir ubuntulatest-sbx/scratch
 
 # load the container; after loading, make any necessary installations and stuff
 # here, we don't need the --nv flag for the gpu; that will be used later when we're only
 # running the container; make sure to use --fakeroot here to have root-like privileges inside the container
-apptainer exec --fakeroot -w --home /scratch/general/vast/$USER/ubuntu2204-sbx-home ubuntu2204-sbx /usr/bin/fish          # install stuff here now; follow your usual setup; no need to use sudo
+apptainer shell --fakeroot -w --home /scratch/general/vast/$USER/ubuntulatest-sbx-home ubuntulatest-sbx          # install stuff here now; follow your usual setup; no need to use sudo
 
 # installing some standard stuff
 apt update
-apt install curl unzip software-properties-common rsync htop nvtop ncdu fzf fish yadm tmux
+apt install curl unzip software-properties-common rsync htop nvtop ncdu fzf fish yadm tmux clang llvm
 apt-get install ca-certificates build-essential
 
-# get dotfiles
-yadm clone https://github.com/codetalker7/dotfiles.git
+# setup fish things
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install PatrickF1/fzf.fish
+# after this, clone the fish config file from your dotfiles (i have a separate config file for the server); see the steps below
 
 # from this point on, you can use your usual dotfiles
 # first, let's do bob and nvim
@@ -27,6 +29,11 @@ curl -fsSL https://raw.githubusercontent.com/MordechaiHadad/bob/master/scripts/i
 fish_add_path $HOME/.local/bin
 bob use latest
 fish_add_path $HOME/.local/share/bob/nvim-bin
+
+# get dotfiles
+yadm clone https://github.com/codetalker7/dotfiles.git
+# in the tmux.conf file, change the tmux prefix to C-m; also, change default
+# shell to fish with: set-option -g default-shell /usr/bin/fish
 
 # tmux
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
@@ -41,4 +48,16 @@ conda init fish
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # finally, exit from the container, and save the image; can load this image as above (not writable)
-apptainer build ubuntu2204-sbx.sif ubuntu2204-sbx
+# also keep copy of image on home
+apptainer build ubuntulatest-sbx.sif ubuntulatest-sbx
+rm -rf ubuntulatest-sbx               # can recreate this from .sif file
+cp ubuntulatest-sbx.sif $HOME/
+cp -r ubuntulatest-sbx-home $HOME/
+
+# can access the container using the sif file now;
+apptainer exec --fakeroot --nv --home /scratch/general/vast/$USER/ubuntulatest-sbx-home ubuntulatest-sbx.sif /usr/bin/fish          # install stuff here now; follow your usual setup; no need to use sudo
+
+# later, can re-build the writeable sandbox from .sif file (to install additional stuff)
+apptainer build --fakeroot --sandbox /scratch/general/vast/$USER/ubuntulatest-sbx ubuntulatest-sbx.sif
+
+# in this re-built sandbox, you can install things again; make sure to save the image again! (follow the steps above)
