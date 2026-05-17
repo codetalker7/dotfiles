2751101ed0d21047c7c4c1b388665559acaf92fa
Siddhant Chaudhary <u1591895@notchpeak2.int.chpc.utah.edu>
2026-05-16 22:29:22 -0600

    minor change to utah chpc config

--- utahchpc/.custom.sh
+++ utahchpc/.custom.sh
@@ -8,13 +8,43 @@
 #forced_session=“screen”
 #forced_session=“tmux”
 
+# colors in bash
+case "$TERM" in
+	xterm-color|*-256color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+#force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+	if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+	else
+	color_prompt=
+	fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+	PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+	PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
+
 # vim mode in bash
 set -o vi
+bind -m vi-insert '"\C-l": clear-screen'
 
 # fzf
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
 # globals for moving cache dirs to scratch
+export SCRATCH_DIR=/scratch/general/vast/$USER
 export PIP_CACHE_DIR=/scratch/general/nfs1/$USER/pip_cache
 export HF_HOME="/scratch/general/vast/$USER/hf_home"
 export UV_CACHE_DIR="/scratch/general/vast/$USER/.cache/uv"
@@ -28,7 +58,7 @@ export APPTAINER_TMPDIR="/scratch/general/vast/$USER/.apptainer/tmp"
 tmux source-file ~/.config/tmux/tmux.conf
 
 # for a fish-like experience, I'm using ble.sh: https://github.com/akinomyoga/ble.sh
-source -- ~/.local/share/blesh/ble.sh
+# source -- ~/.local/share/blesh/ble.sh
 
 # ----------------------------------------------------------------------
 if [[ "$UUFSCELL" = "kingspeak.peaks" ]] ; then
